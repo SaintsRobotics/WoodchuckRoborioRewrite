172f7e783a73ab4b7ecaceb62437fdc282751920
Shreyas <skraman1999@gmail.com>
2015-09-11 02:17:07 -0400

    Example test.

---
+++ test/com/saintsrobotics/treecycle/TestStuff.java
@@ -0,0 +1,10 @@
+package com.saintsrobotics.treecycle;
+
+import junit.framework.TestCase;
+
+public class TestStuff extends TestCase {
+
+    public void testStuff() {
+        assertEquals(true, false);
+    }
+}
