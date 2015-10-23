83eb2fa2baa3732a4f30a21714819e3ae1d05e83
Benjamin <fellfromatree@gmail.com>
2015-10-23 20:29:18 +0000

    added imports to DriveSubsystem

--- src/com/saintsrobotics/woodchuck/subsystems/DriveSubsystem.java
+++ src/com/saintsrobotics/woodchuck/subsystems/DriveSubsystem.java
@@ -1,10 +1,11 @@
 package com.saintsrobotics.woodchuck.subsystems;
 
 import com.saintsrobotics.woodchuck.RobotMap;
+import com.saintsrobotics.woodchuck.util.Motor;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.SpeedController;
-;
+import edu.wpi.first.wpilibj.Jaguar;
 /**
  * Handles the drivetrain of the robot.
  */
